08739036119b0a6f056b426a2c3a89867938d4b6
Amogh Wagh <amoghwagh@gmail.com>
2020-02-03 00:35:44 +0530

    Added router and routes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const Koa = require("Koa");
+const koaRouter = require("koa-router");
 const json = require("koa-json");
 
+const router = new koaRouter();
 const app = new Koa();
 
 app.use(json());
-app.use(async ctx => (ctx.body = "Hello World!"));
+
+router.get("/test", ctx => (ctx.body = "Hello Test"));
+
+app.use(router.routes()).use(router.allowedMethods());
+app.use(async ctx => (ctx.body = { msg: "Hello World!" }));
 
 app.listen(3000, () => console.log("Server Started on port 300ṭ0"));
